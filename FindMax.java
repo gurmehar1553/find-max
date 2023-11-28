3a7bec720aa3360d0efeda2703a4fd27bbf51b2c
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-28 14:03:31 +0530

    Refracted: UC4 created generic method

--- FindMax.java
+++ FindMax.java
@@ -2,15 +2,15 @@ import java.util.*;
 public class FindMax {
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-//            Integer a = sc.nextInt();
-//            Integer b = sc.nextInt();
-//            Integer c = sc.nextInt();
-//            findMax(a,b,c);
-//
-//            Float f1 = sc.nextFloat();
-//            Float f2 = sc.nextFloat();
-//            Float f3 = sc.nextFloat();
-//            findMax(f1,f2,f3);
+            Integer a = sc.nextInt();
+            Integer b = sc.nextInt();
+            Integer c = sc.nextInt();
+            findMax(a,b,c);
+
+            Float f1 = sc.nextFloat();
+            Float f2 = sc.nextFloat();
+            Float f3 = sc.nextFloat();
+            findMax(f1,f2,f3);
 
             String s1 = sc.next();
             String s2 = sc.next();
