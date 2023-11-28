cdbe18af4b0f1a8a760991f82cc812485324d348
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-28 14:13:03 +0530

    Added: UC5 created generic class

--- FindMax.java
+++ FindMax.java
@@ -1,23 +1,32 @@
 import java.util.*;
-public class FindMax {
+public class FindMax <T extends Comparable> {
+        T a,b,c;
+        public FindMax(T a,T b,T c){
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
             Integer a = sc.nextInt();
             Integer b = sc.nextInt();
             Integer c = sc.nextInt();
-            findMax(a,b,c);
+            new FindMax<Integer>(a,b,c).findMax();
 
             Float f1 = sc.nextFloat();
             Float f2 = sc.nextFloat();
             Float f3 = sc.nextFloat();
-            findMax(f1,f2,f3);
+            new FindMax<Float>(f1,f2,f3).findMax();
 
             String s1 = sc.next();
             String s2 = sc.next();
             String s3 = sc.next();
-            findMax(s1,s2,s3);
+            new FindMax<String>(s1,s2,s3).findMax();
         }
-        public static <T extends Comparable> void findMax(T a,T b,T c){
+        private void findMax(){
+            findMax(a,b,c);
+        }
+        public <T extends Comparable> void findMax(T a,T b,T c){
             T ans = a;
             if(ans.compareTo(b) < 0){
                 ans = b;
@@ -27,6 +36,4 @@ public class FindMax {
             }
             System.out.println(ans);
         }
-
-
 }
