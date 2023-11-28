89bcea727fc16f952159f9c11c114a7f7778dfb7
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-28 14:00:29 +0530

    Added: UC4 created generic method

--- FindMax.java
+++ FindMax.java
@@ -2,50 +2,31 @@ import java.util.*;
 public class FindMax {
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
-            Integer a = sc.nextInt();
-            Integer b = sc.nextInt();
-            Integer c = sc.nextInt();
-            findIntMax(a,b,c);
-
-            Float f1 = sc.nextFloat();
-            Float f2 = sc.nextFloat();
-            Float f3 = sc.nextFloat();
-            findFloatMax(f1,f2,f3);
+//            Integer a = sc.nextInt();
+//            Integer b = sc.nextInt();
+//            Integer c = sc.nextInt();
+//            findMax(a,b,c);
+//
+//            Float f1 = sc.nextFloat();
+//            Float f2 = sc.nextFloat();
+//            Float f3 = sc.nextFloat();
+//            findMax(f1,f2,f3);
 
             String s1 = sc.next();
             String s2 = sc.next();
             String s3 = sc.next();
-            findStringMax(s1,s2,s3);
+            findMax(s1,s2,s3);
         }
-        public static void findStringMax(String a,String b,String c){
-            String max = a;
-            if(max.compareTo(b) < 0){
-                max = b;
+        public static <T extends Comparable> void findMax(T a,T b,T c){
+            T ans = a;
+            if(ans.compareTo(b) < 0){
+                ans = b;
             }
-            if(max.compareTo(c) < 0){
-                max = c;
+            if(ans.compareTo(c) < 0){
+                ans = c;
             }
-            System.out.println(max);
+            System.out.println(ans);
         }
-        public static void findFloatMax(Float a,Float b,Float c){
-            Float maxFLoat = a;
-            if(maxFLoat.compareTo(b) < 0){
-                maxFLoat = b;
-            }
-            if (maxFLoat.compareTo(c) < 0){
-                maxFLoat = c;
-            }
-            System.out.println(maxFLoat);
-        }
-        public static void findIntMax(Integer a,Integer b,Integer c){
-            Integer maxInt = a;
-            if(maxInt.compareTo(b) < 0){
-                maxInt = b;
-            }
-            if(maxInt.compareTo(c) < 0){
-                maxInt = c;
-            }
-            System.out.println(maxInt);
-        }
+
 
 }
