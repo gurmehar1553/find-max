493875ef01fdd7d3306e8c19e177d61acaf9a8d9
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-28 13:46:06 +0530

    Added: UC2 find max of 3 floats

--- FindMax.java
+++ FindMax.java
@@ -5,10 +5,23 @@ public class FindMax {
             Integer a = sc.nextInt();
             Integer b = sc.nextInt();
             Integer c = sc.nextInt();
-
             findIntMax(a,b,c);
-        }
 
+            Float f1 = sc.nextFloat();
+            Float f2 = sc.nextFloat();
+            Float f3 = sc.nextFloat();
+            findFloatMax(f1,f2,f3);
+        }
+        public static void findFloatMax(Float a,Float b,Float c){
+            Float maxFLoat = a;
+            if(maxFLoat.compareTo(b) < 0){
+                maxFLoat = b;
+            }
+            if (maxFLoat.compareTo(c) < 0){
+                maxFLoat = c;
+            }
+            System.out.println(maxFLoat);
+        }
         public static void findIntMax(Integer a,Integer b,Integer c){
             Integer maxInt = a;
             if(maxInt.compareTo(b) < 0){
