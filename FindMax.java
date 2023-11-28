d2bef375aaf663951b55b7ef8a5b341fd3df533f
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-28 13:30:25 +0530

    Added: UC1 find max of 3 integers

--- FindMax.java
+++ FindMax.java
@@ -1,5 +1,23 @@
+import java.util.*;
 public class FindMax {
-    public static void main(String[] args){
-        
-    }
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            Integer a = sc.nextInt();
+            Integer b = sc.nextInt();
+            Integer c = sc.nextInt();
+
+            findIntMax(a,b,c);
+        }
+
+        public static void findIntMax(Integer a,Integer b,Integer c){
+            Integer maxInt = a;
+            if(maxInt.compareTo(b) < 0){
+                maxInt = b;
+            }
+            if(maxInt.compareTo(c) < 0){
+                maxInt = c;
+            }
+            System.out.println(maxInt);
+        }
+
 }
